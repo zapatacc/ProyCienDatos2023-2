cf9d42095ad20084191a6061d0e0f032e043e9ca
Cristian Zapata <cristian.zapata@wizeline.com>
2023-08-20 21:50:16 -0600

    Updating class

---
+++ Módulo 1/main.py
@@ -0,0 +1,34 @@
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Union
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+
+app = FastAPI()
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+@app.get("/")
+def root():
+    return {'message': 'Hello World!'}
+
+
+@app.get("/item/{item_id}")
+def read_item(item_id):
+    return {'item id': item_id}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info", reload=False)
